4845f1766ba1cb145fdd62b915b3fc84493aec44
lx <lx354081082@gmail.com>
2018-07-15 20:58:48 +0800

    工作流test

--- src/main/java/com/lx/reptile/ReptileApplication.java
+++ src/main/java/com/lx/reptile/ReptileApplication.java
@@ -3,6 +3,10 @@ package com.lx.reptile;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @MapperScan("com.lx.reptile.mapper")
@@ -10,5 +14,12 @@ public class ReptileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReptileApplication.class, args);
+	}
+
+	@Primary
+	@Bean
+	public TaskExecutor primaryTaskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		return executor;
 	}
 }
---
+++ src/main/java/com/lx/reptile/controller/ActDemoController.java
@@ -0,0 +1,51 @@
+package com.lx.reptile.controller;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequestMapping("/act")
+public class ActDemoController {
+    @Autowired
+    private RepositoryService repositoryService;
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("demo1")
+    public Object demo1() {
+        //根据bpmn文件部署流程
+        Deployment deployment = repositoryService.createDeployment().addClasspathResource("processes/MyAct.bpmn").deploy();
+        //获取流程定义
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().deploymentId(deployment.getId()).singleResult();
+        //启动流程定义，返回流程实例
+        ProcessInstance pi = runtimeService.startProcessInstanceById(processDefinition.getId());
+        String processId = pi.getId();
+        System.out.println("流程创建成功，当前流程实例ID："+processId);
+
+        Task task=taskService.createTaskQuery().processInstanceId(processId).singleResult();
+        System.out.println("第一次执行前，任务名称："+task.getName());
+        taskService.complete(task.getId());
+
+        task = taskService.createTaskQuery().processInstanceId(processId).singleResult();
+        System.out.println("第二次执行前，任务名称："+task.getName());
+        taskService.complete(task.getId());
+
+        task = taskService.createTaskQuery().processInstanceId(processId).singleResult();
+        System.out.println("task为null，任务执行完毕："+task);
+
+        return true;
+    }
+}
